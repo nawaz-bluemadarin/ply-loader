746f2dad078d468e1b3dc35fc68970a31cbf4720
Nawaz Pasha <nawaz.bluemandarin@gmail.com>
2023-09-21 17:46:06 +0530

    play all

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 import { useState, useRef, useEffect } from "react";
+import './App.css'
 
 function App() {
   const containerRef = useRef(null);
@@ -12,9 +13,11 @@ function App() {
   const mandibularRef = useRef(null);
   const maxillaryRef = useRef(null);
   const [cameraPosition, setCameraPosition] = useState({ x: 0, y: 0.15, z: 3 });
+  const [currentModel, setCurrentModel] = useState('Mandibular.ply')
   const [showUpper, setShowUpper] = useState(true);
   const [showLower, setShowLower] = useState(true);
-
+  const mandibularModels = ['Mandibular.ply', 'Mandibular2.ply', 'Mandibular3.ply', 'Mandibular4.ply', 'Mandibular5.ply']
+  const maxillaryModels = ['Maxillary.ply', 'Maxillary2.ply', 'Maxillary3.ply', 'Maxillary4.ply', 'Maxillary5.ply']
   const init = () => {
     const container = containerRef.current;
 
@@ -37,7 +40,8 @@ function App() {
     plane.position.y = -0.5;
     sceneRef.current.add(plane);
 
-    loadPLYModel("Models/Mandibular.ply", 0.02, mandibularRef);
+    console.log(currentModel)
+    loadPLYModel(`Models/${currentModel}`, 0.02, mandibularRef);
     loadPLYModel("Models/Maxillary.ply", 0.02, maxillaryRef);
 
     sceneRef.current.add(new THREE.HemisphereLight(0x443333, 0x000));
@@ -55,10 +59,50 @@ function App() {
       cameraRef.current,
       rendererRef.current.domElement
     );
-    controlsRef.current.minDistance = 3;
+    controlsRef.current.minDistance = 2;
     controlsRef.current.maxDistance = 5;
 
     window.addEventListener("resize", onWindowResize, false);
+  };
+
+  const changeModel = (modelIndex) => {
+    const mandibularModelPath = `Models/${mandibularModels[modelIndex]}`;
+    const maxillaryModelPath = `Models/${maxillaryModels[modelIndex]}`;
+
+    // Remove the current models from the scene
+    if (mandibularRef.current) {
+      sceneRef.current.remove(mandibularRef.current);
+    }
+    if (maxillaryRef.current) {
+      sceneRef.current.remove(maxillaryRef.current);
+    }
+
+    // Load and add the new models
+    loadModel(mandibularModelPath);
+    loadModel(maxillaryModelPath);
+
+    // Set the current model state
+    setCurrentModel(mandibularModels[modelIndex]);
+  };
+
+  const loadModel = (modelPath) => {
+    const loader = new PLYLoader();
+    loader.load(`${modelPath}`, function (geometry) {
+      geometry.computeVertexNormals();
+      const material = new THREE.MeshStandardMaterial({
+        vertexColors: THREE.VertexColors
+      });
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.scale.multiplyScalar(0.02);
+      sceneRef.current.add(mesh);
+
+      // Update the refs
+      if (modelPath.includes('Mandibular')) {
+        mandibularRef.current = mesh;
+      } else if (modelPath.includes('Maxillary')) {
+        maxillaryRef.current = mesh;
+      }
+    });
   };
 
   const loadPLYModel = (filePath, scale, target) => {
@@ -130,6 +174,15 @@ function App() {
     setShowUpper(false);
   };
 
+  const playAll = () => {
+    changeModel(0)
+    for (let i = 0; i < mandibularModels.length; i++) {
+      setTimeout(()=>{
+        changeModel(i)
+      },300)
+    }
+  }
+
   useEffect(() => {
     if (mandibularRef.current) {
       mandibularRef.current.visible = showLower;
@@ -191,7 +244,28 @@ function App() {
           </div>
           <button onClick={toggleLower}>Lower</button>
         </div>
+        <div
+          style={{
+            position: "absolute",
+            top: "92%",
+            right: "75%",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: '10px'
+          }}>
+          {mandibularModels.map((model, index) => (
+            <button key={model} onClick={() => changeModel(index)}>
+              Week {index + 1}
+            </button>
+          ))}
+          <button onClick={() => {
+            playAll()
+          }}>Play</button>
+        </div>
       </div>
+
     </>
   );
 }
